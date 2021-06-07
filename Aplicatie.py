7cb4f98e362d8fe597f1cbf8e8d1e89360d18590
Ladaru Nataly <78505313+ladarunataly42@users.noreply.github.com>
2021-06-07 16:26:22 +0300

    Update Aplicatie.py

--- Aplicatie.py
+++ Aplicatie.py
@@ -7,67 +7,75 @@ class Actions:
 
 #---ACTIONS---
     # VIEW TABLE
-    def printD(a):
-        print(a)
+    def printD(dataframe):
+        print(dataframe)
+        print("Number of rows: {}.".format(int(dataframe.shape[0])))
+        print("Number of columns: {}.".format(int(dataframe.shape[1])))
 
     # DELETE ROW WITH NAN
-    def delete_nan(a):
-        a.dropna(inplace=True)
-        print(a)
+    def delete_nan(dataframe):
+        dataframe.dropna(inplace=True)
+        print(dataframe)
 
     # DELETE DUPLICATES, KEEP FIRST
-    def duplicatef(a):
-        a.drop_duplicates(subset=['Name'], keep='first')
-        print(a)
+    def duplicatef(dataframe):
+        dataframe.drop_duplicates(subset=['Name'], keep='first')
+        print(dataframe)
 
     # DELETE DUPLICATES, KEEP SECOND
-    def duplicates(a):
-        a.drop_duplicates(subset=['Name'], keep='last')
-        print(a)
+    def duplicates(dataframe):
+        dataframe.drop_duplicates(subset=['Name'], keep='last')
+        print(dataframe)
 
     # DELETE SPECIFIED COLUMN
-    def drop_col(a,col_index: int):
+    def drop_col(dataframe,col_index: int):
         if col_index != 0 and col_index != 4:
-            a.drop(a.columns[col_index], inplace=True, axis=1)
+            dataframe.drop(dataframe.columns[col_index], inplace=True, axis=1)
         else:
             print("Nu ai voie sa stergi coloanele respective('Name','Rank')")
-        print(a.columns)
+        print(dataframe.columns)
     
      # DELETE SPECIFIED ROW
-    def drop_row(a,row_index: int):
+    def drop_row(dataframe,row_index: int):
         if row_index != 0:
-            a.drop(row_index, inplace=True)
-            print(a)
+            dataframe.drop(row_index, inplace=True)
+            print(dataframe)
         else:
             print("Nu ai voie sa stergi randul respectiv")
         
 
     # CALCULATE AVERAGE ON COLUMN
-    def average(a,col_index: int):
+    def average(dataframe,col_index: int):
         average_col=0
-        req_col=a.columns[col_index]
-        average_obj=a.mean(skipna=True, numeric_only=True)
+        req_col=dataframe.columns[col_index]
+        average_obj=dataframe.mean(skipna=True, numeric_only=True)
         average_col=average_obj.get(req_col,'Coloana selectata nu are valoare numerica.')
         print(average_col)
 
     # CALCULATE MEDIAN ON COLUMN
-    def median(a,col_index: int):
+    def median(dataframe,col_index: int):
         average_med=0
-        req_col=a.columns[col_index]
-        average_obj_med=a.median(skipna=True, numeric_only=True)
+        req_col=dataframe.columns[col_index]
+        average_obj_med=dataframe.median(skipna=True, numeric_only=True)
         average_med=average_obj_med.get(req_col,'Coloanei nu i se poate calcula mediana.')
         print(average_med)
+    
+    #Describe
+    def describe(dataframe):
+        print(dataframe.describe())
+
+    
 
 class Export:
 
     # EXPORT
-    def exportcsv(a):
+    def exportcsv(dataframe):
         datat.to_csv("New Data-CSV.csv")
 
-    def exportjson(a):
+    def exportjson(dataframe):
         datat.to_json("New Data-JSON.json")
 
-    def exportexcel(a):
+    def exportexcel(dataframe):
         datat.to_excel("New Data-Excel.xlsx")
 
 #---MENU---
@@ -75,52 +83,55 @@ class Export:
 def menu():
         print("Main Choice: Data CLEAN")
         print("Choose 1 - show the table")
-        print("Choose 2 - delete  duplicate,keep the first")
-        print("Choose 3 - delete duplicate,keep the second")
-        print("Choose 4 - delete  NaN")
-        print("Choose 5 - delete column")
-        print("Choose 6 - delete row")
-        print("Choose 7 - calculate average on column")
-        print("Choose 8 - calculate median on column")
-        print("Choose 9 - export csv")
-        print("Choose 10 - export json")
-        print("Choose 11 - export excel")
-        print("Choose 12 - EXIT")
+        print("Choose 2 - describe")
+        print("Choose 3 - delete  duplicate,keep the first")
+        print("Choose 4 - delete duplicate,keep the second")
+        print("Choose 5 - delete  NaN")
+        print("Choose 6 - delete column")
+        print("Choose 7 - delete row")
+        print("Choose 8 - calculate average on column")
+        print("Choose 9 - calculate median on column")
+        print("Choose 10 - export csv")
+        print("Choose 11 - export json")
+        print("Choose 12 - export excel")
+        print("Choose 13 - EXIT")
 menu()
 
 
 #---MAIN---  
 datat=pd.read_csv("DataC.csv")
 choice = input ("Please make a choice: ")
-while choice!=12:
+while choice!=13:
     
     if choice == "1":
         Actions.printD(datat) 
-    elif choice == "2":
-        Actions.duplicatef(datat)
+    elif choice=="2":
+        Actions.describe(datat)
     elif choice == "3":
-        Actions.duplicates(datat)
+        Actions.duplicatef(datat)
     elif choice == "4":
-        Actions.delete_nan(datat)
+        Actions.duplicates(datat)
     elif choice == "5":
+        Actions.delete_nan(datat)
+    elif choice == "6":
         col_index=int(input("Alege indexul coloanei:"))
         Actions.drop_col(datat,col_index)
-    elif choice == "6":
+    elif choice == "7":
         row_index=int(input("Alege indexul randului:"))
         Actions.drop_row(datat,row_index)
-    elif choice == "7":
-        col_index=int(input("Alege indexul coloanei:"))
-        Actions.average(datat,col_index)
     elif choice == "8":
         col_index=int(input("Alege indexul coloanei:"))
-        Actions.median(datat,col_index)    
+        Actions.average(datat,col_index)
     elif choice == "9":
-        Export.exportcsv(datat)
+        col_index=int(input("Alege indexul coloanei:"))
+        Actions.median(datat,col_index)  
     elif choice == "10":
-        Export.exportjson(datat)
+        Export.exportcsv(datat)
     elif choice == "11":
-        Export.exportexcel(datat)   
+        Export.exportjson(datat)
     elif choice == "12":
+        Export.exportexcel(datat)   
+    elif choice == "13":
         exit()        
     else:
         print("I don't understand the choice")
